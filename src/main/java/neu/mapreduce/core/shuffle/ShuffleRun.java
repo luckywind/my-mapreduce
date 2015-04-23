fca65fa5af882c956d34707a77b1d85464f84da8
Mit Mehta <mehta.mit09@gmail.com>
2015-04-23 03:49:05 -0400

    Refactoring ShuffleRun

--- src/main/java/neu/mapreduce/core/shuffle/ShuffleRun.java
+++ src/main/java/neu/mapreduce/core/shuffle/ShuffleRun.java
@@ -4,6 +4,7 @@ import api.JobConf;
 import api.MyWriteComparable;
 import neu.mapreduce.core.combiner.Combiner;
 import neu.mapreduce.core.factory.WriteComparableFactory;
+import neu.mapreduce.io.sockets.IOCommons;
 
 import java.io.*;
 import java.lang.reflect.InvocationTargetException;
@@ -31,128 +32,123 @@ public class ShuffleRun {
 
     /**
      * This function takes a input the output file from the map phase and creates one file for each key.
-     * This file contains the key on the first line and the its values in sorted order. 
+     * This file contains the key on the first line and the its values in sorted order.
      * In-memory sorting is performed*
-     * @param mapperOutputFilePath  Path to mapper's output file
-     * @param locationOfShuffleFiles    Path of the output shuffle files
-     * @param clientJarPath Path to the client JAR
-     * @param jobConf   Instance of JobConf from the client
+     * @param mapperOutputFilePath   Path to mapper's output file
+     * @param locationOfShuffleFiles Path of the output shuffle files
+     * @param clientJarPath          Path to the client JAR
+     * @param jobConf                Instance of JobConf from the client
      */
     public void shuffle(String mapperOutputFilePath, String locationOfShuffleFiles, String clientJarPath, JobConf jobConf) {
-        int shuffleCounter = ZERO;
-        Hashtable<String, ArrayList> keyListOfValue = new Hashtable<>();
-        WriteComparableFactory keyFactory = WriteComparableFactory.generateWriteComparableFactory(jobConf.getMapKeyOutputClassName());
-        WriteComparableFactory valueFactory = WriteComparableFactory.generateWriteComparableFactory(jobConf.getMapValueOutputClassName());
+        WriteComparableFactory mapperOutputKeyFactory = WriteComparableFactory.generateWriteComparableFactory(jobConf.getMapKeyOutputClassName());
+        WriteComparableFactory mapperOutputValueFactory = WriteComparableFactory.generateWriteComparableFactory(jobConf.getMapValueOutputClassName());
         new File(locationOfShuffleFiles).mkdir();
         String mappingFilename = locationOfShuffleFiles + "/" + KEY_FILENAME_MAPPING;
         BufferedWriter filemappingBW = null;
         BufferedReader inputBufferedReader = null;
         try {
             filemappingBW = new BufferedWriter(new FileWriter(new File(mappingFilename)));
-            inputBufferedReader = new BufferedReader(new FileReader(new File(mapperOutputFilePath)));
-            String line;
-            while ((line = inputBufferedReader.readLine()) != null) {
-                String[] keyvalue = line.split(INPUT_FILE_KEYVALUE_SEPARATOR, TWO);
-                if (keyvalue.length < TWO) {
-                    LOGGER.log(Level.WARNING, "SHUFFLE: Ignoring one line as it does not have a key and value. Line:" + keyvalue);
-                    continue;
-                }
-                if (!keyListOfValue.containsKey(keyvalue[KEY_INDEX])) {
-                    keyListOfValue.put(keyvalue[KEY_INDEX], new ArrayList());
-                }
-                try {
-                    keyListOfValue.get(keyvalue[KEY_INDEX]).add(valueFactory.getNewInstance().deserialize(keyvalue[VALUE_INDEX]));
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                } catch (InstantiationException e) {
-                    e.printStackTrace();
-                }
-            }//END OF WHILE
-
-            //POST PROCESSING
-            for (String key : keyListOfValue.keySet()) {
-
-                String locationShuffleFile = locationOfShuffleFiles + "/" + shuffleCounter;
-                BufferedWriter newBW = new BufferedWriter(new FileWriter(new File(locationShuffleFile)));
-
-                Collections.sort(keyListOfValue.get(key));
-                if (jobConf.isIsCombinerSet()) {
-                    keyListOfValue.get(key).iterator();
-                    new Combiner().combinerRun(key, keyListOfValue.get(key).iterator(), keyFactory, clientJarPath, jobConf.getCombinerClassName(), newBW);
-                } else {
-                    writeToFile(newBW, key, keyListOfValue.get(key));
-                    writeToKeyFileMapping(filemappingBW, key, locationShuffleFile);
-                }
-                newBW.flush();
-                newBW.close();
-
-                shuffleCounter++;
-            }
-
-            if (jobConf.isIsCombinerSet()) {
-                LOGGER.log(Level.INFO, "Combiner done!");
-            }
-
-
-            //TODO: Send keyTOBw.keySet() to master
-
+            Hashtable<String, ArrayList> keyListOfValue = readMapperOutput(mapperOutputFilePath, mapperOutputValueFactory);
+            performShuffle(keyListOfValue, locationOfShuffleFiles, clientJarPath, jobConf, mapperOutputKeyFactory, filemappingBW);
         } catch (FileNotFoundException e) {
-            LOGGER.log(Level.SEVERE, "SHUFFLE: Failed as output file of mapper couldn't be found");
-            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, "Failed as output file of mapper couldn't be found");
         } catch (IOException e) {
-            LOGGER.log(Level.SEVERE, "SHUFFLE: Failed as IOException while reading output file from mapper couldn't be found");
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, "Failed as IOException while reading output file from mapper couldn't be found");
+        } catch (ClassNotFoundException | NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            LOGGER.log(Level.SEVERE, "Error while creating an instance of mapper output key or value class");
         } finally {
-            if (filemappingBW != null) {
-                try {
-                    filemappingBW.flush();
-                    filemappingBW.close();
-                    inputBufferedReader.close();
-                } catch (IOException e) {
-                    LOGGER.log(Level.WARNING, "SHUFFLE: Failed to close filemapping bufferedwriter due to IOException");
-                }
-            }
+            IOCommons.shutDownBufferedWriter(filemappingBW);
+            IOCommons.shutDownBufferedReader(inputBufferedReader);
         }
     }
 
+    /**
+     * Creates a Hashtable that maps key with all the values present in the output of mapper
+     * @param mapperOutputFilePath     Location of output of mapper
+     * @param mapperOutputValueFactory Instance of a factory of Mapper output value
+     * @return Hashtable that maps key with all the values present in the output of mapper
+     * @throws IOException
+     */
+    private Hashtable<String, ArrayList> readMapperOutput(String mapperOutputFilePath, WriteComparableFactory mapperOutputValueFactory) throws IOException {
+        Hashtable<String, ArrayList> keyListOfValue = new Hashtable<>();
+        BufferedReader inputBufferedReader = new BufferedReader(new FileReader(new File(mapperOutputFilePath)));
+        String line;
+        while ((line = inputBufferedReader.readLine()) != null) {
+            String[] keyvalue = line.split(INPUT_FILE_KEYVALUE_SEPARATOR, TWO);
+            if (keyvalue.length < TWO) {
+                LOGGER.log(Level.WARNING, "Ignoring one line as it does not have a key and value. Line:" + line);
+                continue;
+            }
+            if (!keyListOfValue.containsKey(keyvalue[KEY_INDEX])) {
+                keyListOfValue.put(keyvalue[KEY_INDEX], new ArrayList());
+            }
+            try {
+                keyListOfValue.get(keyvalue[KEY_INDEX]).add(mapperOutputValueFactory.getNewInstance().deserialize(keyvalue[VALUE_INDEX]));
+            } catch (IllegalAccessException | InstantiationException e) {
+                LOGGER.log(Level.WARNING, "Couldn't create an instance for a line in output of mapper: Line:" + line);
+            }
+        }
+        return keyListOfValue;
+    }
 
+    /**
+     * Takes as input key and corresponding list of values.
+     * Either calls the combiner or sorts the list of values and write the output to appropriate file
+     * It also updates the file that holds the mapping between key and shuffle file output.
+     * @param keyListOfValue         Hashtable that maps key with all the values present in the output of mapper
+     * @param locationOfShuffleFiles Location where shuffle files needs to stored
+     * @param clientJarPath          Location of client JAr
+     * @param jobConf                Instance of JobConf from the client
+     * @param mapperOutputKeyFactory Instance of a factory of Mapper output key
+     * @param filemappingBW          BufferedWriter for fileMapping
+     * @throws IOException
+     * @throws NoSuchMethodException
+     * @throws InvocationTargetException
+     * @throws IllegalAccessException
+     * @throws InstantiationException
+     * @throws ClassNotFoundException
+     */
+    private void performShuffle(Hashtable<String, ArrayList> keyListOfValue, String locationOfShuffleFiles, String clientJarPath, JobConf jobConf, WriteComparableFactory mapperOutputKeyFactory, BufferedWriter filemappingBW) throws IOException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException, ClassNotFoundException {
+        int shuffleCounter = ZERO;
+        for (String key : keyListOfValue.keySet()) {
+            String locationShuffleFile = locationOfShuffleFiles + "/" + shuffleCounter;
+            BufferedWriter shuffleFileBW = new BufferedWriter(new FileWriter(new File(locationShuffleFile)));
+            Collections.sort(keyListOfValue.get(key));
+            if (jobConf.isIsCombinerSet()) {
+                new Combiner().combinerRun(key, keyListOfValue.get(key).iterator(), mapperOutputKeyFactory, clientJarPath, jobConf.getCombinerClassName(), shuffleFileBW);
+            } else {
+                writeToFile(shuffleFileBW, key, keyListOfValue.get(key));
+            }
+            writeToKeyFileMapping(filemappingBW, key, locationShuffleFile);
+            shuffleFileBW.flush();
+            shuffleFileBW.close();
+            shuffleCounter++;
+        }
+    }
+
+    /**
+     * Writes to the file that holds the mapping between the key and output shuffle file*
+     * @param filemappingBW       BufferedWriter for the file that holds the mapping between the key and output shuffle file*
+     * @param key                 the output key of the mapper
+     * @param locationShuffleFile Location of the shuffle file for the given key
+     * @throws IOException
+     */
     private static void writeToKeyFileMapping(BufferedWriter filemappingBW, String key, String locationShuffleFile) throws IOException {
         filemappingBW.write(key + OUTPUT_SHUFFLE_FILE_VALUE_SEPARATOR + locationShuffleFile + OUTPUT_SHUFFLE_FILE_LINE_SEPARATOR);
     }
 
-    private static void writeToFile(BufferedWriter newBW, String key, ArrayList values) throws IOException {
-        newBW.write(key + OUTPUT_SHUFFLE_FILE_LINE_SEPARATOR);
+    /**
+     * Given an key and list of values writes it to file based on the BufferedWriter*
+     * @param fileBW BufferedWriter of the output file
+     * @param key    Key is written as the first line of the output file
+     * @param values Values is a list that is written to the file after the key
+     * @throws IOException
+     */
+    private static void writeToFile(BufferedWriter fileBW, String key, ArrayList values) throws IOException {
+        fileBW.write(key + OUTPUT_SHUFFLE_FILE_LINE_SEPARATOR);
         Iterator iterator = values.iterator();
         while (iterator.hasNext()) {
-            newBW.write(((MyWriteComparable) iterator.next()).getString() + OUTPUT_SHUFFLE_FILE_LINE_SEPARATOR);
+            fileBW.write(((MyWriteComparable) iterator.next()).getString() + OUTPUT_SHUFFLE_FILE_LINE_SEPARATOR);
         }
-        /*for(V value:values){
-            newBW.write(value.getString()+OUTPUT_SHUFFLE_FILE_LINE_SEPARATOR);
-        }*/
     }
-
- /*   private static WriteComparableFactory generateWriteComparableFactory(String classname) {
-        Class keyClass = null;
-        try {
-            keyClass = Class.forName(classname);
-            return new WriteComparableFactory(keyClass);
-        } catch (ClassNotFoundException e) {
-            LOGGER.log(Level.WARNING, "Class not found:" + classname);
-            e.printStackTrace();
-        } catch (InstantiationException | IllegalAccessException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }*/
-
 }
