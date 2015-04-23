25707041375fb34b04b5522fd1fc1450b597fa2a
srikar reddy <demagu.sr@gmail.com>
2015-04-23 00:51:36 -0400

    Adding java docs to Connection Types enum

--- src/main/java/neu/mapreduce/commons/sockets/ConnectionTypes.java
+++ src/main/java/neu/mapreduce/commons/sockets/ConnectionTypes.java
@@ -3,6 +3,10 @@ package neu.mapreduce.commons.sockets;
 /**
  * Created by Amitash on 4/13/15.
  */
+
+/**
+ * ENUM which depicts the slave status to the master
+ */
 public enum ConnectionTypes {
     IDLE, BUSY, JOB_COMPLETE;
 }
